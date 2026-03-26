11605a48721cbfaa37fd4eee41f3f2b826eeba93
DanielLQi <252957718+DanielLQi@users.noreply.github.com>
2026-03-26 14:20:35 +1100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hello from "../components/hello";
+
+export default function Home() {
+  return (
+    <main>
+      <div>Welcome to Next.js!</div>
+      <Hello />
+    </main>
+  );
+}
---
+++ src/components/hello.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Hello() {
+    return (
+        <div>Hello!</div>
+    )
+}
