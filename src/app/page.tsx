a1ca7860cf2fa31302d18aa68966e3cba830c0aa
DanielLQi <252957718+DanielLQi@users.noreply.github.com>
2026-03-29 19:57:46 +1100

    Checkpoint

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+    </section>
+  )
+}
