85eff4f87018a0b975485a3985ad14f882a1cfe0
DanielLQi <252957718+DanielLQi@users.noreply.github.com>
2026-03-29 19:24:48 +1100

    Wiped previous content

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
-  reactCompiler: true,
-  experimental: {
-    turbopackFileSystemCacheForDev: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-
-    return (
-        <div>
-            <h1>Showing details for user number #{id}</h1>
-        </div>
-    );
-}
